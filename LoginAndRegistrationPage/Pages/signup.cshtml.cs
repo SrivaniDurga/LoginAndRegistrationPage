65f937c54664832053946604cb56c88d6aa4e9b9
Srivani Durga <srivanisiri01@gmail.com>
2023-12-27 14:11:02 +0530

    Add project files.

---
+++ LoginAndRegistrationPage/Pages/signup.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoginAndRegistrationPage.Pages
+{
+    public class sinupModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ LoginAndRegistrationPage/Pages/thankyou.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LoginAndRegistrationPage.Pages
+{
+    public class thankyouModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
